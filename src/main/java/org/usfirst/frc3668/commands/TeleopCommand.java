8aa155ca9edaf57c7f6cbd1faf4fe316f4b01030
TroBots <wlhsroboticsteam@gmail.com>
2019-08-05 21:01:39 -0400

    DZ finished hatch solenoid, started cargoFLip solenoid

--- src/main/java/org/usfirst/frc3668/commands/TeleopCommand.java
+++ src/main/java/org/usfirst/frc3668/commands/TeleopCommand.java
@@ -19,7 +19,7 @@ public class TeleopCommand extends Command {
     @Override
     protected void execute() {
         Robot.Chassis.Drive(Robot.oi.joyDrive);
-        
+    
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/main/java/org/usfirst/frc3668/commands/cmdIntakeFlip.java
+++ src/main/java/org/usfirst/frc3668/commands/cmdIntakeFlip.java
@@ -7,11 +7,10 @@
 
 package org.usfirst.frc3668.commands;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Command;
-import org.usfirst.frc3668.Robot;
-public class test extends Command {
-  public test() {
+
+public class cmdIntakeFlip extends Command {
+  public cmdIntakeFlip() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
   }
@@ -19,13 +18,11 @@ public class test extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    Robot.pneumatics.doubleTest(DoubleSolenoid.Value.kOff);
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.pneumatics.doubleTest(DoubleSolenoid.Value.kForward);
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -37,13 +34,11 @@ public class test extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
-    Robot.pneumatics.doubleTest(DoubleSolenoid.Value.kReverse);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
-    end();
   }
 }

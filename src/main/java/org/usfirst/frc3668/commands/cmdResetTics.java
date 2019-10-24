ab73f0611e450ffa1ebce044065919b903195e63
TroBots <wlhsroboticsteam@gmail.com>
2019-10-23 20:34:23 -0400

    DZ added 14000 tic Auto and reversed joy Y on diver stick for better useabillity

---
+++ src/main/java/org/usfirst/frc3668/commands/cmdResetTics.java
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc3668.commands;
+
+import org.usfirst.frc3668.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class cmdResetTics extends Command {
+  public cmdResetTics() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    Robot.Chassis.resetBothEncoders();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.Chassis.resetBothEncoders();
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
--- src/main/java/org/usfirst/frc3668/subsystems/Chassis.java
+++ src/main/java/org/usfirst/frc3668/subsystems/Chassis.java
@@ -7,17 +7,13 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
-import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
-import com.ctre.phoenix.motorcontrol.RemoteLimitSwitchSource;
+
 import org.usfirst.frc3668.Robot;
 import org.usfirst.frc3668.commands.TeleopDrive;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.command.Subsystem;
-
-
 
 public class Chassis extends Subsystem {
 
@@ -30,23 +26,26 @@ public class Chassis extends Subsystem {
     public static final int chassisDriveMaxCurrentLimit = 55;
     public static final int talonTimeOut = 10;
     public static final int chassisDriveMaxCurrentTimeout = 500;
-    public static final int rightDrive1CanID = 2;
-    public static final int rightDrive2CanID = 4;
-    public static final int leftDrive1CanID = 1;
-	public static final int leftDrive2CanID = 5;
+    public static final int rightDrive1CanID = 1;
+    public static final int rightDrive2CanID = 5;
+    public static final int leftDrive1CanID = 4;
+	public static final int leftDrive2CanID = 2;
     public static boolean isDriveInverted = false;
     public final double joyDriveDeadband = 0.05;
     public final double chassisRightSideScalar = 1;
-	public final double chassisLeftSideScalar = 1;
+	public final double chassisLeftSideScalar = 0.92;
 	public final boolean chassisSquareJoyInput = true;
 	public final double chassisBeltReduction = 1.0/1.0;
+	
 	
     
 
     public Chassis() {
         
         PDP = new PowerDistributionPanel(0);
-        addChild("PDP",PDP);
+		
+		
+
         
         
         leftDrive1 = new TalonSRX(leftDrive1CanID);
@@ -66,19 +65,41 @@ public class Chassis extends Subsystem {
         
         leftDrive2 = new TalonSRX(leftDrive2CanID);
         leftDrive2.setNeutralMode(NeutralMode.Brake);
-		leftDrive2.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, Chassis.talonTimeOut);
 		leftDrive2.configPeakCurrentLimit(Chassis.chassisDriveMaxCurrentLimit, Chassis.talonTimeOut);
 		leftDrive2.configPeakCurrentDuration(Chassis.chassisDriveMaxCurrentTimeout, Chassis.talonTimeOut);
         
         
         rightDrive2 = new TalonSRX(rightDrive2CanID);
         rightDrive2.setNeutralMode(NeutralMode.Brake);
-		rightDrive2.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, Chassis.talonTimeOut);
 		rightDrive2.configPeakCurrentLimit(Chassis.chassisDriveMaxCurrentLimit, Chassis.talonTimeOut);
 		rightDrive2.configPeakCurrentDuration(Chassis.chassisDriveMaxCurrentTimeout, Chassis.talonTimeOut);
         
     }
-        
+		
+	public void resetLeftEncoder(){
+		leftDrive1.setSelectedSensorPosition(0,0,0);
+	}
+	public void resetRightEncoder(){
+		rightDrive1.setSelectedSensorPosition(0,0,0);
+	}
+	public void resetBothEncoders(){
+		resetLeftEncoder();
+		resetRightEncoder();
+	}
+	public void runMotors(double throttle){
+		setLeftMotors(throttle);
+		setRightMotors(-throttle);
+	}
+	public int getRightEncoderTics(){
+		return rightDrive1.getSelectedSensorPosition(0);
+	}
+
+	public int getLeftEncoderTics(){
+		return rightDrive1.getSelectedSensorPosition(0);
+	}
+
+	
+
     public void Drive(Joystick stick) {
         double joyX = stick.getX();
         double joyY = stick.getY();
@@ -92,30 +113,35 @@ public class Chassis extends Subsystem {
 		double rightMotorThrottle;
 		double leftMotorThrottle;
 		if (Robot.isDriveInverted) {
-			rightMotorThrottle = (joyX + joyY) * chassisRightSideScalar;
-			leftMotorThrottle = (joyX - joyY) *  chassisLeftSideScalar;
+			rightMotorThrottle = (joyX + -joyY) * chassisRightSideScalar;
+			leftMotorThrottle = (joyX - -joyY) *  chassisLeftSideScalar;
+			
 			if (chassisSquareJoyInput) {
 				double rightSignum = Math.signum(rightMotorThrottle);
 				double leftSignum = Math.signum(leftMotorThrottle);
 				rightMotorThrottle = Math.pow(rightMotorThrottle, 2) * rightSignum;
 				leftMotorThrottle = Math.pow(leftMotorThrottle, 2) * leftSignum;
+				
 			}
             
             setLeftMotors(leftMotorThrottle);
 			setRightMotors(rightMotorThrottle);
+			
+			
 
 		} else {
-			rightMotorThrottle = (joyX - joyY) * chassisRightSideScalar;
-			leftMotorThrottle = (joyX + joyY) * chassisLeftSideScalar;
+			rightMotorThrottle = (joyX - -joyY) * chassisRightSideScalar;
+			leftMotorThrottle = (joyX + -joyY) * chassisLeftSideScalar;
 			if (chassisSquareJoyInput) {
 				double rightSignum = Math.signum(rightMotorThrottle);
 				double leftSignum = Math.signum(leftMotorThrottle);
 				rightMotorThrottle = Math.pow(rightMotorThrottle, 2) * rightSignum;
-				leftMotorThrottle = Math.pow(leftMotorThrottle, 2) * leftSignum;
+				leftMotorThrottle = Math.pow(leftMotorThrottle, 2) * leftSignum ;
 			}
 			
 			setLeftMotors(leftMotorThrottle);
 			setRightMotors(rightMotorThrottle);
+			
 		}
 	}
 
@@ -123,12 +149,15 @@ public class Chassis extends Subsystem {
 		if(Robot.isDriveInverted){
 			setLeftMotors(leftThrottle);
 			setRightMotors(-rightThrottle);
+			
 		} else {
 			setLeftMotors(-leftThrottle);
 			setRightMotors(rightThrottle);
+			
 		}
 	}
 
+	
 	public void setRightMotors(double throttle) {
 		rightDrive1.set(ControlMode.PercentOutput, throttle);
 		rightDrive2.set(ControlMode.PercentOutput, throttle);
@@ -154,7 +183,7 @@ public class Chassis extends Subsystem {
 			setRightMotors(rightMotorThrottle);
 		}
 
-    }
+	}
     
 
     @Override

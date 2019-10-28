5fe8c7feb9836b7d455efa66c3fdfa2872738eec
TroBots <wlhsroboticsteam@gmail.com>
2019-10-28 18:26:46 -0400

    DZ added 113in auto

--- src/main/java/org/usfirst/frc3668/commands/AutonomousCommand.java
+++ src/main/java/org/usfirst/frc3668/commands/AutonomousCommand.java
@@ -9,7 +9,7 @@ import org.usfirst.frc3668.subsystems.Chassis;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class AutonomousCommand extends Command {
-    double tthrottle = .3;
+    double tthrottle = .4;
 
     public AutonomousCommand() {
         requires(Robot.Chassis);
@@ -26,13 +26,14 @@ public class AutonomousCommand extends Command {
     protected void execute() {
         System.err.println("TICS: "+  Robot.Chassis.getRightEncoderTics() +" ,Right " + Robot.Chassis.getLeftEncoderTics());
         
-
-        if (Robot.Chassis.rightDrive1.getSelectedSensorPosition() < 14000){
+        if (Robot.Chassis.rightDrive1.getSelectedSensorPosition() < 113 * 100){
             Robot.Chassis.runMotors(tthrottle);
         }
-        else if (Robot.Chassis.rightDrive1.getSelectedSensorPosition() >= 14000){
+        else if (Robot.Chassis.rightDrive1.getSelectedSensorPosition() >= 113 * 100){
             Robot.Chassis.runMotors(0);
         }
+    
+        
     }
 
    
